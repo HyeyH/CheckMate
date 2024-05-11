2d24d1cd3db6b5f3efd3614ac1df9aac84fdddf4
hyhan <hhy33719@gmail.com>
2024-05-11 18:03:07 +0900

    data_window 수정 중

--- gui/data_window.py
+++ gui/data_window.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QDialog, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QTreeView, QFileSystemModel, QMessageBox, QFileDialog, QDialogButtonBox, QRadioButton, QTextEdit
+import random
+from PyQt5.QtWidgets import QApplication, QDialog, QLabel, QVBoxLayout, QHBoxLayout, QPushButton, QTreeView, QFileSystemModel, QMessageBox, QFileDialog, QDialogButtonBox, QLineEdit, QRadioButton, QComboBox, QTextEdit
 from PyQt5.QtGui import QFont, QIcon, QFontDatabase
 from PyQt5.QtCore import Qt, QDir
 import sys, os
@@ -96,8 +97,108 @@ class DataWindow(QDialog):
         self.setLayout(hbox_layout)
 
     def open_ratio_data(self):
-        None
+        dialog = QDialog()
+        dialog.resize(500, 400)
+        layout = QVBoxLayout()
+
+        self.directory_combo = QComboBox()
+        self.populate_directory_combo()
+        layout.addWidget(self.directory_combo)
         
+        # 비율을 지정하기 위한 레이블과 입력 필드를 추가합니다.
+        self.ratio_label = QLabel("데이터 분할 비율을 선택해 주세요. 순서대로 train/valid/test 입니다.")
+        layout.addWidget(self.ratio_label)
+
+        self.radio_602020 = QRadioButton("60/20/20")
+        self.radio_603010 = QRadioButton("60/30/10")
+        self.radio_702010 = QRadioButton("70/20/10")
+        radio_layout = QHBoxLayout()
+        radio_layout.addWidget(self.radio_602020)
+        radio_layout.addWidget(self.radio_603010)
+        radio_layout.addWidget(self.radio_702010)    
+        layout.addLayout(radio_layout)
+
+        # 분할 버튼을 추가합니다.
+        self.split_button = QPushButton("데이터 분할")
+        self.split_button.clicked.connect(self.split_data)
+        layout.addWidget(self.split_button)
+
+        dialog.setLayout(layout) 
+        dialog.exec_()
+
+    def populate_directory_combo(self):
+        # './data' 디렉터리에서 디렉터리 명들을 읽어와 콤보박스에 추가합니다.
+        directory = './data'
+        directories = [d for d in os.listdir(directory) if os.path.isdir(os.path.join(directory, d))]
+        self.directory_combo.addItems(directories)
+
+    def split_data(self):
+        directory_name = self.directory_combo.currentText()
+        if self.radio_603010.isChecked():
+            ratio_text = "60:30:10"
+        elif self.radio_602020.isChecked():
+            ratio_text = "60:20:20"
+        elif self.radio_702010.isChecked():
+            ratio_text = "70:20:10"
+        else:
+            QMessageBox.warning(self, "경고", "원하는 분할 비율을 선택하세요.")
+            return
+        try:
+            # 입력된 비율을 ':'로 분할하여 train, valid, test 비율로 나눕니다.
+            ratios = ratio_text.split(':')
+            if len(ratios) != 3:
+                QMessageBox.warning(self, "Error", "올바른 비율을 입력하세요.")
+                return
+            train_ratio, valid_ratio, test_ratio = map(float, ratios)
+            total_ratio = train_ratio + valid_ratio + test_ratio
+
+            # './data' 디렉터리에 train, valid, test 디렉터리를 생성합니다.
+            train_dir = os.path.join('./data', directory_name, 'train')
+            valid_dir = os.path.join('./data', directory_name, 'valid')
+            test_dir = os.path.join('./data', directory_name, 'test')
+            os.makedirs(train_dir, exist_ok=True)
+            os.makedirs(valid_dir, exist_ok=True)
+            os.makedirs(test_dir, exist_ok=True)
+
+            # './data/{directory_name}' 디렉터리의 이미지 파일들을 무작위로 나누어 train, valid, test에 복사합니다.
+            source_dir_good = os.path.join('./data', directory_name, 'images', 'good')
+            source_dir_bad = os.path.join('./data', directory_name, 'images', 'bad')
+            filenames_good = os.listdir(source_dir_good)
+            filenames_bad = os.listdir(source_dir_bad)
+
+            # good과 bad 디렉토리에 있는 파일 이름을 모두 합칩니다.
+            filenames = filenames_good + filenames_bad
+            random.shuffle(filenames)
+
+            train_count = int(train_ratio / total_ratio * len(filenames))
+            valid_count = int(valid_ratio / total_ratio * len(filenames))
+            test_count = len(filenames) - train_count - valid_count
+
+            train_files = filenames[:train_count]
+            valid_files = filenames[train_count:train_count + valid_count]
+            test_files = filenames[train_count + valid_count:]
+
+            for filename in train_files:
+                shutil.copy(os.path.join(source_dir_good, filename), os.path.join(train_dir, filename))
+            for filename in valid_files:
+                shutil.copy(os.path.join(source_dir_good, filename), os.path.join(valid_dir, filename))
+            for filename in test_files:
+                shutil.copy(os.path.join(source_dir_good, filename), os.path.join(test_dir, filename))
+
+            for filename in train_files:
+                shutil.copy(os.path.join(source_dir_bad, filename), os.path.join(train_dir, filename))
+            for filename in valid_files:
+                shutil.copy(os.path.join(source_dir_bad, filename), os.path.join(valid_dir, filename))
+            for filename in test_files:
+                shutil.copy(os.path.join(source_dir_bad, filename), os.path.join(test_dir, filename))
+                
+            QMessageBox.information(self, "완료", "데이터 분할이 완료되었습니다.")
+
+        except Exception as e:
+            QMessageBox.warning(self, "Error", str(e))
+            return
+        
+
     def save_yaml_file(self, file_path, data):
         with open(file_path, 'w') as file:
             yaml.dump(data, file)
@@ -109,8 +210,7 @@ class DataWindow(QDialog):
     
     def open_label_data(self):
         dialog = QDialog()
-        dialog.resize(400, 500)
-        
+        dialog.resize(500, 400)
         layout = QVBoxLayout()
         label = QLabel("데이터를 라벨링할 물품의 이름을 하나만 입력하세요. 예) eraser")
         layout.addWidget(label)
@@ -119,20 +219,31 @@ class DataWindow(QDialog):
         button_ok = QPushButton("확인")
 
         def on_button_ok_clicked():
-            item_name = text_edit.toPlainText().strip()  # text_edit의 텍스트를 가져옴 (양쪽 공백 제거)
+            item_name = text_edit.text().strip()  # text_edit의 텍스트를 가져옴 (양쪽 공백 제거)
             if not item_name:
                 QMessageBox.warning(self, "경고", "물품명을 입력하세요.")
                 return
             else:
-                directory = os.path.join("data/"+item_name, 'labels')
-                os.makedirs(directory, exist_ok=True)  # 디렉터리가 이미 있으면 오류를 발생시키지 않음
-                QMessageBox.information(self, "완료", f"해당 물품명 디렉터리에 labels 폴더가 생성되었습니다.\n경로: {directory}")
-                dialog.close()
+                image_directory = os.path.join("data", item_name, "images")
+                good_directory = os.path.join(image_directory, "good")
+                bad_directory = os.path.join(image_directory, "bad")
+                
+                # good 디렉터리가 존재하고 비어 있지 않은지 확인
+                if os.path.exists(good_directory) and os.listdir(good_directory):
+                    QMessageBox.information(self, "확인", "Good 디렉터리에 이미지가 있습니다.")
+                    dialog.close()
+                # bad 디렉터리가 존재하고 비어 있지 않은지 확인
+                elif os.path.exists(bad_directory) and os.listdir(bad_directory):
+                    QMessageBox.information(self, "확인", "Bad 디렉터리에 이미지가 있습니다.")
+                    dialog.close()
+                else:
+                    QMessageBox.warning(self, "경고", "Good 또는 Bad 디렉터리에 이미지가 있어야 합니다.")
+                    return
+                    
         button_ok.clicked.connect(on_button_ok_clicked)
         layout.addWidget(button_ok)
         dialog.setLayout(layout)
         dialog.exec_()
-        
         # exe_path = os.path.join("labelImg", 'labelImg.py')
         # if os.path.exists(exe_path) and os.access(exe_path, os.X_OK):
         #     process = subprocess.Popen(['python', exe_path])
@@ -147,17 +258,15 @@ class DataWindow(QDialog):
             process.wait()
         else:
             QMessageBox.warning(self, "Error", "데이터 라벨링을 진행할 수 없습니다.")
-        
 
     def open_add_data(self):
         dialog = QDialog(self)
         dialog.setFont(QFont(self.font_family))
         dialog.setWindowTitle("데이터 추가")
-        dialog.resize(500, 500)
+        dialog.resize(500, 400)
         item_label = QLabel("추가할 데이터의 이름을 영문으로 작성해 주세요. 예) eraser, milk")
 
         self.item_name = QTextEdit()  
-        self.item_name.setFixedHeight(30)
         item_layout = QVBoxLayout()
         item_layout.addWidget(item_label)
         item_layout.addWidget(self.item_name)
@@ -215,11 +324,11 @@ class DataWindow(QDialog):
         directory = os.path.join('data', item_name_text)
         os.makedirs(directory, exist_ok=True)
         
-        # 정상 또는 불량에 따라 images/good 또는 images/bad 디렉터리 생성
+        # 정상 또는 불량에 따라 good 또는 bad 디렉터리 생성
         if self.radio_good.isChecked():
-            directory = os.path.join(directory, "images", "good")
+            directory = os.path.join(directory, "good")
         elif self.radio_bad.isChecked():
-            directory = os.path.join(directory, "images", "bad")
+            directory = os.path.join(directory, "bad")
         else:
             QMessageBox.warning(self, "경고", "데이터 종류를 선택하세요.")
             return
@@ -231,10 +340,16 @@ class DataWindow(QDialog):
             return
         else:
             # 선택된 파일들을 복사해서 해당 디렉터리로 이동
-            for file in self.files:
-                shutil.copy(file, directory)
+            for i, file in enumerate(self.files):
+                # 파일명과 확장자를 분리
+                filename, extension = os.path.splitext(file)
+                # 새로운 파일명 생성
+                new_filename = 'g{}'.format(i) if self.radio_good.isChecked() else 'b{}'.format(i)
+                new_filepath = os.path.join(directory, new_filename + extension)
+                shutil.copy(file, new_filepath)
         
         QMessageBox.information(self, "완료", "데이터 추가가 완료되었습니다.")
+
 
     def open_file_dialog(self):
         options = QFileDialog.Options()
